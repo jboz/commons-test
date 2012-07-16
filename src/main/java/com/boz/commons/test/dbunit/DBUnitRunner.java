6124e69e4b2a6e93d36879513e24194872dc2555
jboz <julienboz@gmail.com>
2012-07-16 18:46:43 +0200

    latest version + prepare to release

--- src/main/java/com/boz/commons/test/dbunit/DBUnitRunner.java
+++ src/main/java/com/boz/commons/test/dbunit/DBUnitRunner.java
@@ -13,7 +13,6 @@ import org.junit.runners.model.Statement;
 
 import com.boz.commons.test.ReflectionUtils;
 
-
 /**
  * Runner JUnit pour utiliser DBUnit.
  * 
@@ -21,47 +20,111 @@ import com.boz.commons.test.ReflectionUtils;
  */
 public class DBUnitRunner extends BlockJUnit4ClassRunner {
 
-  private static EntityManager entityManager;
+	private static EntityManager entityManager;
 
-  public DBUnitRunner(final Class<?> klass) throws InitializationError {
-    super(klass);
-  }
+	public DBUnitRunner(final Class<?> klass) throws InitializationError {
+		super(klass);
+	}
 
-  @Override
-  protected Statement methodInvoker(final FrameworkMethod method, final Object testObject) {
+	@Override
+	protected Statement methodInvoker(final FrameworkMethod method, final Object testObject) {
+		return new DBUnitStatement(method, testObject, super.methodInvoker(method, testObject));
+	}
 
-    final Statement base = super.methodInvoker(method, testObject);
+	/**
+	 * Junit statement that inject EntityManager and start transaction (if required) before test and close EntityManager and
+	 * commit/rollback transaction (if required) after test.
+	 * 
+	 * @author jboz
+	 */
+	private static class DBUnitStatement extends Statement {
 
-    return new Statement() {
+		private final FrameworkMethod method;
+		private final Object testObject;
+		private final Statement base;
 
-      @Override
-      public void evaluate() throws Throwable {
-        final Transactionnal transactionnal = ReflectionUtils.getMethodOrClassLevelAnnotation(Transactionnal.class,
-            method.getMethod(), testObject.getClass());
-        try {
-          // test start
-          entityManager = injectEntityManager(testObject);
-          insertDataSet(entityManager, method.getMethod(), testObject);
+		public DBUnitStatement(final FrameworkMethod method, final Object testObject, final Statement base) {
+			super();
+			this.method = method;
+			this.testObject = testObject;
+			this.base = base;
+		}
 
-          if (transactionnal != null) {
-            // lancement de la transaction
-            entityManager.getTransaction().begin();
-          }
+		@Override
+		public void evaluate() throws Throwable {
+			aroundTest(base, method, testObject, true);
+		}
 
-          base.evaluate();
+	}
 
-        } finally {
-          if (entityManager != null && transactionnal != null && entityManager.getTransaction().isActive()) {
-            if (transactionnal.rollback()) {
-              entityManager.getTransaction().rollback();
-            } else {
-              entityManager.getTransaction().commit();
-            }
-          }
-          // test end if not ignored
-          closePersistenceContext(entityManager);
-        }
-      }
-    };
-  }
+	/**
+	 * Execute test with DBUnit environment.
+	 * 
+	 * @param statement : test to execute
+	 * @param method : test method
+	 * @param testObject : tested object
+	 * @throws Throwable
+	 */
+	public static void aroundTest(final Statement statement, final FrameworkMethod method, final Object testObject)
+			throws Throwable {
+		aroundTest(statement, method, testObject, false);
+	}
+
+	/**
+	 * Execute test with DBUnit environment.
+	 * 
+	 * @param statement : test to execute
+	 * @param method : test method
+	 * @param testObject : tested object
+	 * @param forceDBUnit : force initialize DBUnit context
+	 * @throws Throwable
+	 */
+	public static void aroundTest(final Statement statement, final FrameworkMethod method, final Object testObject,
+			final boolean forceDBUnit) throws Throwable {
+		// ne pas utiliser DBUnit si le test ne le demande pas
+		final boolean dbUnitTest = forceDBUnit || isDBUnitTest(method, testObject);
+
+		final Transactionnal transactionnal = ReflectionUtils.getMethodOrClassLevelAnnotation(Transactionnal.class,
+				method.getMethod(), testObject.getClass());
+		try {
+			if (dbUnitTest) {
+				// test start
+				entityManager = injectEntityManager(testObject);
+				insertDataSet(entityManager, method.getMethod(), testObject);
+
+				if (transactionnal != null) {
+					// lancement de la transaction
+					entityManager.getTransaction().begin();
+				}
+			}
+			statement.evaluate();
+
+		} catch (final Exception e) {
+			// rollback on exception
+			if (dbUnitTest && entityManager != null && transactionnal != null && entityManager.getTransaction().isActive()) {
+				entityManager.getTransaction().rollback();
+			}
+			throw e;
+		} finally {
+			if (dbUnitTest) {
+				if (entityManager != null && transactionnal != null && entityManager.getTransaction().isActive()) {
+					if (transactionnal.rollback()) {
+						entityManager.getTransaction().rollback();
+					} else if (!entityManager.getTransaction().getRollbackOnly()) {
+						entityManager.getTransaction().commit();
+					}
+				}
+				// test end if not ignored
+				closePersistenceContext(entityManager);
+			}
+		}
+	}
+
+	/**
+	 * @return true si le test requiet DBUnit.
+	 */
+	private static boolean isDBUnitTest(final FrameworkMethod method, final Object testObject) {
+		return testObject.getClass().getAnnotation(DataSet.class) != null || method.getMethod().getAnnotation(DataSet.class) != null
+				|| method.getMethod().getAnnotation(Transactionnal.class) != null;
+	}
 }
